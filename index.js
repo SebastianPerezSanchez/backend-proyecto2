354c83ab9c6e779d3b93bad5a23d238cd84d7a89
SebastianPerezSanchez <72109827+SebastianPerezSanchez@users.noreply.github.com>
2022-09-22 09:01:08 -0500

    10

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use( express.json() );
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'https://risad-proyect.web.app');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
