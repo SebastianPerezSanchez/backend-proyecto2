ad6e9af64cd1b4e5d3908069daf0af53ab32c3c7
SebastianPerezSanchez <72109827+SebastianPerezSanchez@users.noreply.github.com>
2022-11-03 05:49:54 -0500

    stringify

--- index.js
+++ index.js
@@ -207,7 +207,7 @@ app.post('/webhook', express.json(),function(request, response){
         let contenidoFull = {
           richContent: [
             [
-              JSON.parse(contenidoInventario)
+              JSON.stringify(contenidoInventario)
             ]
           ]
         };
