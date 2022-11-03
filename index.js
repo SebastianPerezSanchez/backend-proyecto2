dc8c8f9bbc2581d2dc250da0754f89e5863ccd38
SebastianPerezSanchez <72109827+SebastianPerezSanchez@users.noreply.github.com>
2022-11-03 15:40:29 -0500

    idk2

--- index.js
+++ index.js
@@ -213,8 +213,7 @@ app.post('/webhook', express.json(),function(request, response){
             ]
           ]
         };
-        const dfMessenger = document.querySelector('df-messenger');
-        dfMessenger.renderCustomCard(contenidoInventario);
+        agent.add(new Payload(agent.UNSPECIFIED, contenidoFull, {sendAsMessage: true, rawPayload: true}))
         console.log(contenidoFull);
       }
     }
